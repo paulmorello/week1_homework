7645ea1c6d825104a5361d07cb9b0590fa0d4ec3
paulmorello <paulmorello1@gmail.com>
2016-09-28 14:59:10 +1000

    Working with Objects for homework

--- paulmorello/day_2/index.js
+++ paulmorello/day_2/index.js
@@ -2,7 +2,7 @@
 
 var userColor = prompt('What is your favorite color?');
 
-console.log('Your favortite color is ' + userColor);
+console.log('Your favorite color is ' + userColor);
 
 // Write a for loop that will iterate from 0 to 10. For each iteration of the for loop, it will multiply the number by 9 and log the result (e.g. "2 * 9 = 18")
 for (var i = 0; i <= 10; i++) {
